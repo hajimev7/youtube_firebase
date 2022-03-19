81c790a58ca5f5cce643e5dd2cab41a2c592e6c6
hajimev7 <57899416+hajimev7@users.noreply.github.com>
2022-03-19 23:27:07 +0900

    youtube-react

---
+++ src/components/VideoDetail/VideoDetail.js
@@ -0,0 +1,22 @@
+import React, { useContext} from 'react'
+import { Store } from '../../store/index'
+import VideoPlay from '../VideoPlay/VideoPlay'
+import Style from './VideoDetail.module.scss'
+import Linkify from 'react-linkify/dist/components/Linkify'
+
+const VideoDetail=()=> {
+const {globalState} = useContext(Store)
+
+  return globalState.selected.id && globalState.selected.id ?(
+    <div className={Style.wrap} >
+        <VideoPlay id={globalState.selected.id}/>
+        <p>{globalState.selected.snippet.title}</p>
+        <hr />
+        <Linkify>
+          <pre>{globalState.selected.snippet.description}</pre>
+        </Linkify>
+    </div>
+  ) : (<span>no data</span>)
+}
+
+export default VideoDetail
